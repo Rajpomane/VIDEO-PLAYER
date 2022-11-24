8c4a335691d6099e97d52bcc12d557aedb8f79e6
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-11-25 00:17:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,35 @@ SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", ".")
 app = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Modules"))
 
+import asyncio
+import time
+
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from pyrogram import Client
+
+import config
+
+loop = asyncio.get_event_loop()
+boot = time.time()
+
+mongo = MongoClient(config.MONGO_DB_URI)
+db = mongo.AFK
+
+appid = 0
+appame = ""
+appusername = ""
+
+cleanmode = {}
+
+
+getme = await app.get_me()
+appid = getme.id
+appusername = (getme.username).lower()
+if getme.last_name:
+  appname = getme.first_name + " " + getme.last_name
+else:
+  appname = getme.first_name
+
 
 
 
