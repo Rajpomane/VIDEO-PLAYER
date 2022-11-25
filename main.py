b8f71e241df06de748a5fe715fcc0098830d1c37
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-11-25 10:56:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from datetime import datetime
 import dotenv
 
 
-async def _human_time_duration(seconds):
-MONGO_DB_URI = os.getenv("MONGO_DB_URI", None)  
+async def _human_time_duration(seconds): 
 API_ID = int(os.getenv("API_ID", "6"))
 API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 SESSION = os.getenv("SESSION")
