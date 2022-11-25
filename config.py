d067f66b463e0e9010b1d33c49e9334734237efb
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-11-25 12:34:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,59 +7,27 @@ from pyrogram import Client, filters, idle
 from pyrogram.types import Message
 
 from dotenv import load_dotenv
-from datetime import datetime
 import dotenv
 import os
 import asyncio
-import sys
 import time
 
 from dotenv import load_dotenv
 from pyrogram import Client, filters
-from pytgcalls import PyTgCalls
 
-__version__ = "v0.1"
-
-if os.path.exists(".env"):
-    load_dotenv(".env")
-    
-
-# For Local Deploy
-if os.path.exists(".env"):
-    load_dotenv(".env")
 
 # Necessary Vars
+async def _human_time_duration(seconds):
+MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", ".")
-SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
-ALIVE_PIC = os.getenv("ALIVE_PIC", "")
-ALIVE_MSG = os.getenv("ALIVE_MSG", "")
-PING_MSG = os.getenv("PING_MSG", "")
-LOGS_CHANNEL = os.getenv("LOGS_CHANNEL", None)
 
-contact_filter = filters.create(
-    lambda _, __, message: (message.from_user and message.from_user.is_contact)
-    or message.outgoing
-)
-# SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "1517994352 1789859817").split())))
-#----------------------------------------------
-bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Modules"))
-call_py = PyTgCalls(bot)
-
-# Terms
-# Privacy
-# Security
-
-hl = HNDLR[0]
-start_time = time.time()
+app = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Modules"))
 
 
 
-async def _human_time_duration(seconds):
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)  
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SESSION = os.getenv("SESSION")
-HNDLR = os.getenv("HNDLR", ".")
+
+
+
