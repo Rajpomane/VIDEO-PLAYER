7d1835633750d8c968cac944959f32efe91e6acd
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-11-25 12:35:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,11 @@ import re
 import asyncio
 from pyrogram import Client, filters, idle
 from pyrogram.types import Message
-from config import config
+from config import config, app
 from dotenv import load_dotenv
 from datetime import datetime
 import dotenv
 
-
-async def _human_time_duration(seconds): 
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SESSION = os.getenv("SESSION")
-HNDLR = os.getenv("HNDLR", ".")
-app = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Modules"))
 
 import asyncio
 import time
